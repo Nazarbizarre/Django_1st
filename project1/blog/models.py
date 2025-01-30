8e0e5b6bf515a35cca8a4e92b57f99b2f7a2617d
Nazar Bezpalyi <nazar.bez007@gmail.com>
2025-01-30 19:34:50 +0100

    models added

--- project1/blog/models.py
+++ project1/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=250)
+    body = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, 
+        choices=Status,
+        default=Status.DRAFT
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    
+    class Meta:
+        ordering = ['-created']
+        indexes= [
+            models.Index(fields=['-created'])
+        ]
+        
+    def __str__(self):
+        return self.title
+    
