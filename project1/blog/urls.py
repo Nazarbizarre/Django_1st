947c81384b1646b49b981774490e65cbe1809ccc
Nazar Bezpalyi <nazar.bez007@gmail.com>
2025-02-07 11:42:16 +0100

    createpost

--- project1/blog/urls.py
+++ project1/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import home, post_info
+from .views import home, post_info, get_posts_by_category, create_post
 
 app_name = 'blog'
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('post/<int:post_id>', post_info, name="post")
+    path('post/<int:post_id>', post_info, name="post_info"),
+    path("category/<int:category_id>", get_posts_by_category, name="get_posts_by_category"),
+    path('create_post/', create_post, name="create_post")
 ]
--- project1/blog/views.py
+++ project1/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Post
 
 
@@ -11,7 +12,29 @@ def home(request):
 
 def post_info(request, post_id):
   post = get_object_or_404(Post, id=post_id)
-  return render(request, "post.html", {'post':post})
+  return render(request, "post_info.html", {'post':post})
   
+def get_posts_by_category(request, category_id):
+  categories = Category.objects.all()
+  category = get_object_or_404(Category, id=category_id)
+  posts = Post.objects.filter(category=category)
+  data = {"categories":categories, "posts":posts}
+  return render(request, 'index.html', context=data)
   
- 
+def create_post(request):
+  categories = Category.objects.all()
+  if request.method == 'POST':
+    title = request.POST.get('title')
+    body = request.POST.get('body')
+    category_id = request.POST.get('category')
+    category = get_object_or_404(Category, id=int(category_id))
+    try:
+      post = Post(title=title, body=body, category=category)
+      post.full_clean()    
+    except:
+      return HttpResponse('<h1>Bad Values</h1>')
+    else:
+      post.save()
+      
+    return redirect('blog:home')
+  return render(request, "create_post.html", context={"categories":categories})
